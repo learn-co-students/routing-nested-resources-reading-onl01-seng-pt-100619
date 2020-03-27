94b9eed992f65039ca744a0689edbe6970e6451f
github username <github email address>
2020-03-26 22:52:32 -0700

    Done.

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,10 @@
 Rails.application.routes.draw do
-  get 'authors/:id/posts', to: 'authors#posts_index'
-  get 'authors/:id/posts/:post_id', to: 'authors#post'
-  resources :authors, only: [:posts_index, :show, :post]
+  resources :authors, only: [:show] do
+    resources :posts, only: [:show, :index]
+  end
 
-  # resources :posts, only: [:index, :show, :new, :create, :edit, :update]
+  resources :posts, only: [:index, :show, :new, :create, :edit, :update]
 
-  # root 'posts#index'
+  root 'posts#index'
 
 end
